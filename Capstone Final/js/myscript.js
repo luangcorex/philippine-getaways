dab315c342d63da7ec7f1f0506306550b144213a
luangco.rex <rexluangco@gmail.com>
2018-03-04 17:51:05 +0800

    mar4edit

--- Capstone Final/js/myscript.js
+++ Capstone Final/js/myscript.js
@@ -1,1 +1,10 @@
-new WOW().init();
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var scroll = $(window).scrollTop();
+    if (scroll > 300) {
+      $(".black").css("background", "blue");
+    } else {
+      $(".black").css("background", "#333");
+    }
+  });
+});
